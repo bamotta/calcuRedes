9cc99b58d350a946f653f5a47196d518bf8c30cb
bamotta <abdamotta@gmail.com>
2025-05-13 17:21:51 +0200

    comprobación de numeros en rango correcto

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 
-function calcultae(){
-    
+function startValidationOctects() {
+  const octetos = ['octect1', 'octect2', 'octect3', 'octect4'];
+  octetos.forEach(id => {
+    const input = document.getElementById(id);
+    input.addEventListener('input', () => validationOctect(input));
+  });
+}
+
+function validationOctect(input){
+    const valor = parseInt(input.value,10);
+    input.style.borderColor = (!isNaN(valor) && valor >= 0 && valor <= 255) ? 'green' : 'red';
+}
+
+window.addEventListener('DOMContentLoaded', startValidationOctects);
+
+function calculate(){
+
 }
